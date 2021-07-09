6989f644d6e997546b1bdc5c2a2e8a91e3c27f24
Angry Hamster <62916663+KsenijaBabarokina@users.noreply.github.com>
2021-07-09 20:21:41 +0200

    Two problems from session 3
    MaxIntProduct -> dynamic programming
    Common Child -> Substrings/HashMap/Stack

---
+++ HackerRank/CommonChild.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result2 {
+
+    /*
+     * Complete the 'commonChild' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static int commonChild(String a, String b) {
+        // Write your code here
+        int[][] C = new int[a.length()+1][b.length()+1];
+
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = 0; j < b.length(); j++) {
+                if (a.charAt(i) == b.charAt(j)) {
+                    C[i+1][j+1] = C[i][j] + 1;
+                } else {
+                    C[i+1][j+1] = Math.max(C[i+1][j], C[i][j+1]);
+                }
+            }
+        }
+
+        return C[a.length()][b.length()];
+
+    }
+
+}
+
+public class CommonChild {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s1 = bufferedReader.readLine();
+
+        String s2 = bufferedReader.readLine();
+
+        int result = Result2.commonChild(s1, s2);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
